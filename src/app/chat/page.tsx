aad2eeb1252cd3dba84fd1ced989ad14f2f39192
rajaryan <rajaryan150149@gmail.com>
2026-03-20 16:38:15 +0530

    feat: initial commit

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+function ChatPage(){
+    return (
+        <div>
+            <h1>Chat</h1>
+        </div>
+    )
+}
+
+export default ChatPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    
+    <div className="flex">
+      <div className="font-extrabold h-screen max-w-max font-stretch-200% bg-amber-200"> 
+         hello
+      </div>
       
     </div>
   );
